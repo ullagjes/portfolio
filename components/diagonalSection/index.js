d7de7f830a9342b5665fbfc690793a62141c25d6
ullagjes <ullagjes@gmail.com>
2021-06-16 12:16:03 +0200

    bug fix

--- components/diagonalSection/index.js
+++ components/diagonalSection/index.js
@@ -4,13 +4,13 @@ import { useMediaQuery } from '@material-ui/core';
 
 function DiagonalSection({children}) {
 
-    const classes = useStyles();
-    const mediumScreen = useMediaQuery(`(max-width:800px)`)
-
-    return (
-        <section className={mediumScreen ? `${classes.smallContainer} + ${classes.container}` : `${classes.container} + ${classes.bigContainer}`}>
-          {children}  
-        </section>
+  const classes = useStyles();
+  const mediumScreen = useMediaQuery(`(max-width:800px)`)
+  
+  return (
+    <section className={mediumScreen ? `${classes.smallContainer} + ${classes.container}` : `${classes.container} + ${classes.bigContainer}`}>
+      {children}  
+    </section>
     );
 }
 
